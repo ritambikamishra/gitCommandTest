96c54d5324052a8ed7e76adc0d71b617817c450a
ritambika mishra <ritambika.mishra@gmail.com>
2021-09-03 14:01:39 +0530

    Implemented sub fn

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ def add(x,y):
     return x+y
 #subtract implementation
 def subtract(x,y):
-    pass
+    return x-y         #on bug456
 #multiply implementation
 def multiply(x,y):
     pass
 #divide implementation
 def divide(x,y):
-    pass
+    passgit branch -d bug124
